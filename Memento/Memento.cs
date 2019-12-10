8bd6e481372539ebbde0a997fc21a81243d2a206
Dmytro Zepsen <Dmytro.Zepsen@thomsonreuters.com>
2019-12-10 13:59:38 +0200

    add memento

---
+++ Memento/Memento.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sky.Memento
+{
+    public interface IMemento
+    {
+        int[] GetState();
+    }
+
+    public class MapState : IMemento
+    {
+        private readonly int[] _state;
+        public MapState(int[] state)
+        {
+            this._state = (int[])state.Clone();
+        }
+
+        public int[] GetState()
+        {
+            return this._state;
+        }
+    }
+
+    public class Map
+    {
+        private int[] map;
+
+        public Map(int[] map)
+        {
+            this.map = map;
+        }
+
+        public IMemento Save()
+        {
+            return new MapState(this.map);
+        }
+
+        internal void Display()
+        {
+            for (int i = 0; i < this.map.Length; i++)
+            {
+                Console.Write(this.map[i]);                
+            }
+            Console.WriteLine();
+            Console.WriteLine(new string('-',20));
+        }
+
+        public void Restore(IMemento memento)
+        {
+            this.map = memento.GetState();
+        }
+
+        public void Set(int x, int val)
+        {
+            this.map[x] = val;
+        }
+    }
+
+    public class MapHistory
+    {
+        private Stack<IMemento> history = new Stack<IMemento>();
+        private Map origin;
+
+        public MapHistory(Map origin)
+        {
+            this.origin = origin;
+        }
+
+        public void Backup()
+        {
+            history.Push(origin.Save());
+        }
+
+        public void Restore()
+        {
+            var map = history.Pop();
+            this.origin.Restore(map);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sky.Memento;
+using System;
 
 namespace Sky
 {
@@ -6,13 +7,30 @@ namespace Sky
     {
         static void Main(string[] args)
         {
-            var map = new Brain();									
-			map.RunDefault(new [] { 4,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0});
+   //         var map = new Brain();									
+			//map.RunDefault(new [] { 4,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0});
             
-			map.Show();
+			//map.Show();
 
-            map.Back();
-            map.Show();
+   //         map.Back();
+   //         map.Show();
+            
+
+            var map = new Map(new int[ ]{ 0,0,0, 0,0,0, 0,0,0 });
+            var history = new MapHistory(map);
+
+            map.Display();
+
+            map.Set(1, 2);
+            map.Set(2, 3);                                    
+            history.Backup();
+            map.Display();
+
+            map.Set(0, 8);
+            map.Display();            
+
+            history.Restore();
+            map.Display();
         }
     }
 }
